bda4f7505593882e20fe599a15337be8d25331cb
ArEl3wam <arelawam@gmail.com>
2022-05-20 13:54:18 +0200

    functionPlotter repo

---
+++ FunctionPlotterFunctions.cpp
@@ -0,0 +1,133 @@
+#pragma once
+#include <string>
+#include <math.h>
+#include <queue>
+#include <stack>
+
+
+bool isOperand(std::string c){
+
+    if(c=="+" || c=="-" || c=="*" || c=="/" || c== "^"){
+        return true;
+    }
+    return false;
+}
+
+
+double evaluator(std::string num1 , std::string num2 ,std::string operand){
+    double a = stod(num1);
+    double b = stod(num2);
+    if(operand== "+"){
+        return a + b;
+    }
+    if(operand== "-"){
+        return a - b;
+    }
+    if(operand== "/"){
+        return a / b;
+    }
+    if(operand== "*"){
+        return a * b;
+    }
+    if(operand== "^"){
+        return pow(a,b);
+    }
+
+}
+
+bool higherPrio(char op1 , char op2){
+
+    if((op1 == '+') && ((op2== '+') || (op2== '-') || (op2== '*') || (op2== '/') || (op2== '^'))){
+        return true;
+    }
+    if((op1 == '-') && ((op2== '+') || (op2== '-') || (op2== '*') || (op2== '/') || (op2== '^'))){
+        return true;
+    }
+    if ( (op1== '*') && ( (op2 == '*') || (op2 == '/') || (op2 == '^') )){
+        return true;
+    }
+    if ( (op1== '/') && ( (op2 == '*') || (op2 == '/') || (op2 == '^') )){
+        return true;
+    }
+    return false;
+
+}
+
+std::queue<std::string> postfix (std::string equation){
+
+    std::queue<std::string> postfix;
+    std::string number="";
+    std::stack<char> operandStack;
+    for(int i=0; i<equation.size(); i++){
+        // operand
+        if(isOperand(std::string(1,equation[i]))){
+            postfix.push(number);
+            number="";
+
+            //stack is empty
+            if(operandStack.empty()){
+                operandStack.push(equation[i]);
+            }
+            //stack not stable
+            else if(higherPrio(equation[i],operandStack.top())){
+                while(!operandStack.empty() && higherPrio(equation[i],operandStack.top())){
+                    postfix.push(std::string(1,operandStack.top()));
+                    operandStack.pop();
+                }
+                operandStack.push(equation[i]);
+
+            }
+            //stack is stable
+            else{
+                operandStack.push(equation[i]);
+            }
+        }
+        // number
+        else{
+            number+=equation[i];
+        }
+    }
+    postfix.push(number);
+    while(!operandStack.empty()){
+        postfix.push(std::string(1,operandStack.top()));
+        operandStack.pop();
+    }
+    return postfix;
+
+
+}
+
+double equationSolver(std::queue<std::string> postFix){
+
+    std::stack<std::string> temp;
+    while(!postFix.empty()){
+        //number
+        if(!isOperand(postFix.front())){
+            temp.push(postFix.front());
+            postFix.pop();
+        }
+        //operand
+        else{
+
+            std::string num2=temp.top();
+            temp.pop();
+            std::string num1=temp.top();
+            temp.pop();
+            temp.push(std::to_string(evaluator(num1,num2,postFix.front())));
+            postFix.pop();
+        }
+    }
+    return (stod(temp.top()));
+
+}
+std::string replacer(std::string equation, std::string maxmin){
+
+    std::string newEquation=equation;
+
+    for(int i=0; i<newEquation.size(); i++){
+        if(newEquation[i]=='x' || newEquation[i]=='X'){
+            newEquation.replace(i,1,maxmin);
+        }
+    }
+    return newEquation;
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,89 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QString"
+#include "FunctionPlotterFunctions.cpp"
+#include <QMessageBox>
+#include <string>
+#include <sstream>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+const char* ConvertDoubleToString(double value){
+    std::stringstream ss;
+    ss << value;
+    const char* str = ss.str().c_str();
+    return str;
+}
+
+void MainWindow::on_Solve_clicked()
+{
+
+    //taking parameters
+    QString eq = ui->equationLine->text();
+    QString maxi = ui->maximumLine->text();
+    QString mini = ui->minimumLine->text();
+    std::string equation = eq.toStdString();
+    std::string maximum= maxi.toStdString();
+    std::string minimum =mini.toStdString();
+
+
+    double maxiNum =maxi.toDouble();
+    double miniNum=mini.toDouble();
+    double increment = (maxiNum-miniNum)/1000;
+    //QMessageBox::information(this,tr("maximum number is"),tr(ConvertDoubleToString(increment)));
+
+    double temp = miniNum;
+    QVector<double> xValues;
+    xValues.push_back(temp);
+    QVector<double> yValues;
+    yValues.push_back(equationSolver(postfix(replacer(equation,minimum))));
+    for(int i=0; i<1000; i++){
+        temp+=increment;
+        std::string dummy = std::to_string(temp);
+        xValues.push_back(temp);
+        double tempY=equationSolver(postfix(replacer(equation,dummy)));
+        yValues.push_back(tempY);
+
+    }
+
+    MainWindow::makePlot(xValues, yValues);
+
+    //double num= equationSolver(postfix(replacer(equation,maximum)));
+    //QString qstring = QString::number(num);
+    //QMessageBox::information(this,tr("maximum number is"),tr(ConvertDoubleToString(equationSolver(postfix(replacer(equation,maximum))))));
+    //cout << equationSolver(postfix(replacer(equation,minimum))) << endl;
+    //cout << equationSolver(postfix(replacer(equation,maximum)));
+}
+
+void MainWindow::makePlot(QVector<double> xValues , QVector<double>yValues){
+
+    // generate some data:
+    /*QVector<double> x(101), y(101); // initialize with entries 0..100
+    for (int i=0; i<101; ++i)
+    {
+      x[i] = i/50.0 - 1; // x goes from -1 to 1
+      y[i] = x[i]*x[i]; // let's plot a quadratic function
+    }*/
+    // create graph and assign data to it:
+    ui->customPlot->addGraph();
+    ui->customPlot->graph(0)->setData(xValues, yValues);
+    // give the axes some labels:
+    ui->customPlot->xAxis->setLabel("x");
+    ui->customPlot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->customPlot->xAxis->setRange(xValues[0], xValues[xValues.size()-1]);
+    ui->customPlot->yAxis->setRange(yValues[0], yValues[yValues.size()-1]);
+    ui->customPlot->replot();
+
+}
